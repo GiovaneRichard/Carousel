f3b1dcb35e67e147c220c6d9d825e6131f38a8a0
Giovane Richard <giovanerichard@gmail.com>
2022-10-12 09:16:56 -0300

    finally javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
 const slides = document.querySelectorAll('.slide');
 const carousel = document.querySelector('#carousel');
+const left = document.querySelector('#left');
+const right = document.querySelector('#right');
+
+const SLIDES_COUNT = slides.length;
+
+let current_slide = 0;
+
+left.addEventListener('click', () => {
+    current_slide--;
+    if(current_slide < 0) {
+        current_slide = SLIDES_COUNT - 1;
+    }
+    updateCarousel();
+});
+
+
+right.addEventListener('click', () => {
+    current_slide++;
+    if(current_slide > SLIDES_COUNT - 1) {
+        current_slide = 0;
+    }
+    updateCarousel();
+});
+
+const updateCarousel = () => {
+    carousel.style.transform = `translateX(${
+        -current_slide * slides[0].offsetWidth
+    }px)`;
+
+    document.body.style.background = `#${slides[current_slide].getAttribute("data-bg")}`;
+}
